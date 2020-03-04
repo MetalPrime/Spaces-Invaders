d5338e45a6f150488ebf144f09ff8a792a5a5181
Jhon Garcia <metalprime57@gmail.com>
2020-03-04 00:18:45 -0500

    update StartScreen

--- src/view/StartScreen.java
+++ src/view/StartScreen.java
@@ -1,5 +1,32 @@
 package view;
 
-public class StartScreen {
+import processing.core.PApplet;
 
+public class StartScreen {
+	
+	private PApplet app;
+	
+	public StartScreen(PApplet app) {
+		this.app = app;
+		
+	}
+	
+	public void paint() {
+		app.background(150);
+		app.textSize(32);
+		app.text("Space Invaders", app.width/3,app.height/3);
+		botton();
+	 
+	}
+	
+	public void clicked() {
+		
+	}
+	
+	public void botton() {
+		int sizeX = 200;
+		int sizeY = 50;
+		app.rect(sizeX,sizeY, app.width*2/3, app.height*2/3);
+	}
 }
+
