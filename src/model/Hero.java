e34bd79cf8e4424669b223c7735b2ad63de454f4
Jhon Garcia <metalprime57@gmail.com>
2020-03-03 23:58:54 -0500

    update hero and invaders

--- src/model/Hero.java
+++ src/model/Hero.java
@@ -1,5 +1,31 @@
 package model;
 
-public class Hero {
+import processing.core.PApplet;
+import view.Characters;
+
+public class Hero extends Characters {
+	
+	
+	
+	public Hero(PApplet app, float x, float y, int size, int movX, int movY) {
+		super(app, x, y, size, movX, movY);
+		// TODO Auto-generated constructor stub
+		
 
 }
+
+	@Override
+	public void paint() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+	
--- src/model/Invaders.java
+++ src/model/Invaders.java
@@ -1,5 +1,25 @@
 package model;
 
-public class Invaders {
+import processing.core.PApplet;
+import view.Characters;
+
+public class Invaders extends Characters {
+
+	public Invaders(PApplet app, float x, float y, int size, int movX, int movY) {
+		super(app, x, y, size, movX, movY);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void paint() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
