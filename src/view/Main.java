c04bc99133f0fc5d2421afc064b1ccde398e7338
Jhon Garcia <metalprime57@gmail.com>
2020-03-03 19:26:28 -0500

    update Main

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,10 +1,31 @@
 package view;
 
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PApplet.main(Main.class.getName());
 	}
-
+	
+	public void settings() {
+		size(800,800);
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void keyPressed() {
+		
+	}
+	
+	public void mousePressed() {
+		
+	}
 }
