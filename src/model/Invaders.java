4746fde27b42ff4df5a1c4b13e2a520cd5040877
Jhon Garcia <metalprime57@gmail.com>
2020-03-07 06:43:12 -0500

    update invaders

--- src/model/Invaders.java
+++ src/model/Invaders.java
@@ -8,18 +8,26 @@ public class Invaders extends Characters {
 	public Invaders(PApplet app, float x, float y, int size, int movX, int movY) {
 		super(app, x, y, size, movX, movY);
 		// TODO Auto-generated constructor stub
+		this.app = app;
+		this.x = app.random(0,800);
+		this.y = -10;
+		this.size = size;
+		this.movX = movX;
+		this.movY = movY;
 	}
 
 	@Override
 	public void paint() {
 		// TODO Auto-generated method stub
+		app.fill(0,0,255);
+		app.rect(x,y,size,size);
 		
 	}
 
 	@Override
 	public void move() {
 		// TODO Auto-generated method stub
-		
+		y+=movY;
 	}
 
 }
--- src/model/Logic.java
+++ src/model/Logic.java
@@ -1,25 +1,29 @@
 package model;
 
-import java.util.ArrayList;
 
 import processing.core.PApplet;
-import view.StartScreen;
 
 public class Logic {
 	
 	private PApplet app;
 	private Hero hero;
-	private ArrayList<Invaders> invaders;
-	
+	private Invaders[] is; 
 
 	public Logic(PApplet app) {
 		this.app = app;
 		 hero = new Hero(app, app.width/2, app.height*4/5, 100, 10, 0);
+		 is = new Invaders[60];
+		 for(int i= 0; i< is.length;i++) {
+			 is[i] = new Invaders(app, 0, 0, 20, 0, 15);
+		 }
 	}
 	
 	public void paint() {
 		hero.paint();
-		
+		 for(int i= 0; i< is.length;i++) {
+			 is[i].paint();
+			 is[i].move();
+		 }
 	}
 
 	public void move() {
